cc9b0d497b58eae83f8f4128076cc5a234df4999
Kevin Bernard <kevos.directos@gmail.com>
2021-03-26 12:48:04 +0100

    Ajout sinusoïde

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,27 @@ function fadeOverlay() {
         {visibility: "hidden", delay: 1}, //create a 0.5 second gap
       ], ease: "power3.inOut"});
 }
+var wrapper = document.querySelector(".svg-wrapper");
+TweenLite.defaultEase = Sine.easeInOut;
+TweenLite.set("g", {x:10, y:30});
+
+var svg   = document.querySelector("svg");
+var wave  = document.querySelector("#wave");
+var width = 350;
+
+var amplitude = 30;
+var frequency = 15;
+var segments  = 200;
+var interval  = width / segments;
+
+for (var i = 0; i < segments; i++) {
+  
+  var norm  = i / segments;  
+  var point = wave.points.appendItem(svg.createSVGPoint());
+  
+  point.x = i * interval;
+  point.y = amplitude / 2;
+  
+  TweenMax.to(point, 0.8, { y: -point.y, repeat: -1, yoyo: true }).progress(norm * frequency);
+}
+
