434a82a2612f3825aac19c41d2d55a939ccbc411
Kevin Bernard <kevos.directos@gmail.com>
2021-05-31 17:38:24 +0200

    Test email

---
+++ email.php
@@ -0,0 +1,13 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "kevin.bernard@zoho.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $headers = "From:" . $from;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    }
+?>
